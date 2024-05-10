482bc1065e188641e83205f26a0041d0467902a0
GuilhermeMutao <guilhermemutao@gmail.com>
2024-05-09 21:34:51 -0300

    wip: constants styles

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,17 @@
 export const COLORS = {
   primary: '#1DB954', 
-  secondary: '#191414',
+  secondary: '#121212',
   white: '#FFFFFF',
-  cardBackground: '#282828'
+  cardBackground: '#282828',
+  gray: '#B3B3B3',
+  darkGray: '#3A3E3B',
 };
 
 export const FONTS = {
   large: 20,
+  mediumLarge: 18,
   medium: 16,
+  smallMedium: 14,
   small: 12,
   title: 28
 };
@@ -16,4 +20,5 @@ export const SPACING = {
   large: 20,
   medium: 16,
   small: 12,
+  extraSmall: 10
 };
