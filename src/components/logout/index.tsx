25d9fb0281eea9981bfa8ede81754dae3cb83c6d
Guilherme Mutão <guilherme.mutao@bravolog.com.br>
2024-07-23 10:49:20 -0300

    fix: import react on logout

--- src/components/logout/index.tsx
+++ src/components/logout/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { View, Text, TouchableOpacity, Modal, Alert } from "react-native";
 import { Icon } from "native-base";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
